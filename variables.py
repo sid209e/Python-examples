e218c537a5aa5c367e889ade826f246409e4e9bf
Siddharth Bishnoi <sidbishnoi48@gmail.com>
2023-02-26 03:03:59 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+#declare variables
+num = 20
+area=19.2
+city = 'Bangalore'
+country = 'India'
+
+name = None     #Declaring variables without assigning values
+
+global var_global
+var_global = 10
+
+print(num)
+print(area)
+print(city)
+print(country)
+print(var_global)
